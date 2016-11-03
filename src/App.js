7a59099f7a895f22f14f9e6757487a1295ec060a
pawelpaszki <pawelpaszki@gmail.com>
2016-11-03 20:06:29 +0000

    update and delete exercise added

--- src/App.js
+++ src/App.js
@@ -584,15 +584,15 @@ var AddExerciseForm = React.createClass({
     },
 	handleSubmit: function(e) {
 		e.preventDefault();
-		console.log(this.props.muscles);
+		//console.log(this.props.muscles);
 		var muscles = _.pluck(this.props.muscles, 'name');
 		var exercises = _.pluck(this.props.exercises, 'name');
 		var name = this.state.name;
 		var group = this.state.group;
-		console.log(muscles);
-		console.log(exercises);
-		console.log(name);
-		console.log(group);
+		//console.log(muscles);
+		//console.log(exercises);
+		//console.log(name);
+		//console.log(group);
 		if (!name || !group || muscles.indexOf(group) == -1 || exercises.indexOf(name) != -1) {
 			this.setState({name: ''});
 			this.setState({group: ''});
@@ -617,7 +617,7 @@ var AddExerciseForm = React.createClass({
                      placeholder="Exercise group"
                      value={this.state.group}
                      onChange={this.handleGroupChange} /></td>
-				<td className="col-md-3"><input type="button" className="btn btn-primary" value="Add muscle"
+				<td className="col-md-3"><input type="button" className="btn btn-primary" value="Add exercise"
 							   onClick={this.handleSubmit} /> </td>
 							   <td className="col-md-1"></td>
 				
@@ -727,25 +727,89 @@ var MuscleList = React.createClass({
 });
 
 var Exercise = React.createClass({
-	render: function() {
-		var exercise = this.props.exercise;
+	getInitialState : function() {
+		 return {
+			 status: '',
+		  name: this.props.exercise.name,
+		  group: this.props.exercise.group,
+		  initName: this.props.exercise.name,
+		  initGroup: this.props.exercise.group,
+		} ;
+	  },
+	handleNameChange: function(e) {
+	   this.setState({name: e.target.value});
+	},
+	handleGroupChange: function(e) {
+	   this.setState({group: e.target.value});
+	},
+	handleDelete: function(e) {
+		e.preventDefault();
+		var exerciseConstants = _.pluck(this.props.exerciseConstants, 'name');
+		var name = this.state.name;
+		var group = this.state.group;
+		//console.log(muscleConstants);
+		//console.log(muscleConstants.indexOf(name));
+		//console.log(name);
+		if (exerciseConstants.indexOf(name) == -1) {
+	       this.props.deleteExerciseHandler(name, group);
+		};
+	},
+	handleEdit: function(e) {
+		this.setState({ status : 'edit'} )
+	},
+	handleUpdate: function(e) {
+	   e.preventDefault();
+	   var name = this.state.name;
+	   var group = this.state.group;
+	   var muscles = _.pluck(this.props.muscles, 'name');
+	   console.log(this.state.initName);
+	   var exerciseConstants = _.pluck(this.props.exerciseConstants, 'name');
+	   console.log(exerciseConstants.indexOf(this.state.initName));
+	   if ((exerciseConstants.indexOf(this.state.initName)) === -1 && (muscles.indexOf(group)) !== -1) {
+	       this.props.updateExerciseHandler(name, group);
+	   } else {
+		   this.setState({ name : this.state.initName});
+		   this.setState({ group : this.state.initGroup});
+	   }
+	   this.setState({ status : ''} )
+	},
+	render:function(){
+		var updateHandler = this.handleUpdate;
+		var editHandler = this.handleEdit;
+		var deleteHandler = this.handleDelete;
+		var itemsToRender;
+		if(this.state.status == '') {
+			itemsToRender = [
+			<table className="table table-borderless">
+				<tbody>
+				  <tr>
+					<td key={'name'} className="col-md-6"><a href={"/sessions/" + this.state.name}>{this.state.name} </a></td>
+					<td className="col-md-2"><input type="button"  className="btn btn-primary btn-block" value="edit" onClick={editHandler}/></td>
+					<td className="col-md-2"><input type="button"  className="btn btn-warning btn-block" value="delete" onClick={deleteHandler}/></td>
+					<td className="col-md-2"></td>
+				  </tr>
+				</tbody>
+			</table>
+			];
+		} else {
+			itemsToRender = [
+			<table className="table table-borderless">
+				<tbody className="center">
+				  <tr>
+					<td key={'name'} className="col-md-4"><input type="text" className="form-control"  value={this.state.name} onChange={this.handleNameChange}/></td>
+					<td key={'group'} className="col-md-4"><input type="text" className="form-control"  value={this.state.group} onChange={this.handleGroupChange}/></td>
+					<td className="col-md-2"><input type="button" className="btn btn-success btn-block" value="confirm" onClick={updateHandler}/></td>
+					<td className="col-md-2"></td>
+				  </tr>
+				</tbody>
+			</table>
+			];
+		};
 		return (
-		
-		<li>
-		<div className="left-within-main">
-<table className="table table-borderless">
-    <tbody>
-      <tr>
-	      <td className="col-md-6"><a  href={"/exercises/" + exercise.name}>{exercise.name} </a></td>
-    <td className="col-md-2"><input type="button"  className="btn btn-primary btn-block" value="edit"/></td>
-    <td className="col-md-2"><input type="button"  className="btn btn-warning btn-block" value="delete"/></td>
-    <td className="col-md-2"></td>
-      </tr>
-    </tbody>
-  </table>
-			 </div>
-		</li>
-		)
+			 <li className="left-within-main">
+				{itemsToRender}
+			 </li>
+			 )
 	}
 });
 
@@ -753,7 +817,8 @@ var Exercise = React.createClass({
 var ExerciseList = React.createClass({
 	render: function() {
 		var Exercises = this.props.exercises.map((exercise) =>{
-			return <Exercise key={exercise.name} exercise={exercise} />;
+			return <Exercise key={exercise.name} exercise={exercise} deleteExerciseHandler={this.props.deleteExerciseHandler}
+			updateExerciseHandler={this.props.updateExerciseHandler} exerciseConstants={this.props.exerciseConstants} muscles={this.props.muscles} />;
 		}) ;
             return (
 			<div className="main-content-without-search-box">
